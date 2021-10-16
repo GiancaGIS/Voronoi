0af6d92aed4ac721f6646179d7f6c6dd5e01f412
Giancarlo Messina <giancarlomex@outlook.com>
2021-10-17 00:48:01 +0200

    Add Code for ArcMap AddIn.

---
+++ ArcMapAddinVoronoi/Forms/FormChooseFeatureLayer.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace ArcMapAddinVoronoi.Forms
+{
+    partial class FormChooseFeatureLayer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxFeatureLayers = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxFeatureLayers
+            // 
+            this.listBoxFeatureLayers.FormattingEnabled = true;
+            this.listBoxFeatureLayers.ItemHeight = 16;
+            this.listBoxFeatureLayers.Location = new System.Drawing.Point(12, 12);
+            this.listBoxFeatureLayers.Name = "listBoxFeatureLayers";
+            this.listBoxFeatureLayers.Size = new System.Drawing.Size(529, 212);
+            this.listBoxFeatureLayers.TabIndex = 0;
+            this.listBoxFeatureLayers.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(416, 248);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FormChooseFeatureLayer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 294);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBoxFeatureLayers);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FormChooseFeatureLayer";
+            this.Text = "FormChooseFeatureLayer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxFeatureLayers;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ ArcMapAddinVoronoi/Forms/FormChooseFeatureLayer.cs
@@ -0,0 +1,55 @@
+﻿using ESRI.ArcGIS.Carto;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace ArcMapAddinVoronoi.Forms
+{
+    public partial class FormChooseFeatureLayer : Form
+    {
+        private IFeatureLayer2 _fLayerChosen = null;
+        public event EventHandler FeatureLayerChosenEventHandler;
+
+        /// <summary>
+        /// Costruttore della form
+        /// </summary>
+        public FormChooseFeatureLayer(List<ILayer2> listFeatureLayerName)
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.listBoxFeatureLayers.DisplayMember = "Name";
+            this.listBoxFeatureLayers.ValueMember = "FeatureLayer";
+
+            foreach (ILayer2 layer in listFeatureLayerName)
+            {
+                this.listBoxFeatureLayers.Items.Add(new FeatureLayerItemListBox()
+                {
+                    Name = layer.Name,
+                    FeatureLayer = (IFeatureLayer2)layer
+                });
+            }
+            this.Refresh();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this._fLayerChosen = ((FeatureLayerItemListBox)this.listBoxFeatureLayers.SelectedItem).FeatureLayer;
+        }
+
+        public IFeatureLayer2 FeatureLayer()
+        {
+            return this._fLayerChosen;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.FeatureLayerChosenEventHandler?.Invoke(this, e);
+        }
+    }
+
+    public class FeatureLayerItemListBox
+    {
+        public string Name { get; set; }
+        public IFeatureLayer2 FeatureLayer { get; set; }
+    }
+}
